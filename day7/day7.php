31e99116bbd843d15f4197faaf52833602c746fb
Majora-OX <61722617+Majora-OX@users.noreply.github.com>
2024-12-10 10:22:30 +1100

    Cleanup

--- day7/day7.php
+++ day7/day7.php
@@ -1,9 +1,9 @@
 <?php
 
-    $input = file("input.txt", 2);
-    $testNumbers = [];
-    $totalResult = 0;
-    foreach($input as $line){
+     $input = file("input.txt", 2);
+     $testNumbers = [];
+     $totalResult = 0;
+     foreach($input as $line){
      preg_match('/[0-9]+:/',$line, $matches);
      preg_match('/[0-9]+/',$matches[0], $NumberOnly1);
      $num1 = $NumberOnly1[0];
@@ -16,16 +16,11 @@
      $operatorsNumber = (count($num2) - 1);
      $combinations = pow(2, $operatorsNumber);
      $loopcombinations = strlen(decbin($combinations - 1));
-    
- 
+     
      do {
         $calculatedNumber = 0;
         $operatorBits = sprintf("%0".$loopcombinations."b",$combinations - 1); #decbin($combinations - 1);
         $bitsArray = str_split($operatorBits,1);
-        #echo "LoopCombo ";
-        #var_dump($loopcombinations);
-        #echo "OperatorBits: ";
-        #var_dump($operatorBits);
         foreach($num2 as $index=>$number){
            
             if($index == 0){
@@ -40,29 +35,13 @@
                     }
 
             }
-          
-          /*  if(($index) != $operatorsNumber ){
-                $positionNo = $index == 0 ? $num2[$index] : $calculatedNumber;
-
-                if($bitsArray[$index] == "0"){
-                $calculatedNumber += ($positionNo + $num2[$index + 1]);
-                }
-                elseif($bitsArray[$index] == "1"){
-                $calculatedNumber += ($positionNo * $num2[$index + 1]);
-                } */
-          #if($num1 == 3267) { echo "$number "; var_dump($calculatedNumber,$positionNo,$num2[$index + 1]);}  
-           // }
-        }  
-        
+        }          
         if($calculatedNumber == $num1){
             echo "$num1\n";
             $totalResult += $num1;
             $combinations = 1;
         }
-
         $combinations -= 1;
      } while($combinations > 0);
-    
-
     }
     echo "$totalResult\n";
